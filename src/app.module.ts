2609477ae0e90479fd83fbcd2e94db1134b264f6
Mar-er <243958407@qq.com>
2020-05-17 21:38:17 +0800

    feat(shared): 将shared模块整体导出

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,12 +6,14 @@ import { AppService } from './app.service';
 import { ConfigModule } from './config/config.module';
 import { Configuration } from './config';
 import { UserModule } from './shared/mongodb/user/user.module';
+import { SharedModule } from './shared/shared.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot(Configuration.get('mongodb_url'), { useNewUrlParser: true }),
     ConfigModule,
-    UserModule
+    UserModule,
+    SharedModule
   ],
   controllers: [AppController],
   providers: [AppService],
