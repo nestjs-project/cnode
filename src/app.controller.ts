76631715e34afca7c8b6f775377b36c6c60fb176
Mar-er <243958407@qq.com>
2020-05-17 21:46:56 +0800

    feat(views、app.controller): 添加修改模板

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -7,11 +7,11 @@ export class AppController {
   constructor(private readonly appService: AppService, private readonly userService: UserService) {}
 
   @Get()
-  // @Render('index')
+  @Render('index')
   async root () {
-    const docs = await this.userService.findAll({});
+    // const docs = await this.userService.findAll({});
     return {
-      data: docs
+      // data: docs
     }
   }
 }
