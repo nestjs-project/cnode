820a395b550a1030adebba6713a45eb9778a2e35
Mar-er <243958407@qq.com>
2020-05-17 21:31:22 +0800

    feat(shared):新增mongodb、user通用服务schema配置基础interface

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,17 @@
 import { Controller, Get, Render } from '@nestjs/common';
 import { AppService } from './app.service';
+import { UserService, User } from './shared/mongodb/user';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService, private readonly userService: UserService) {}
 
   @Get()
-  @Render('index')
-  root() {
-    return {}
+  // @Render('index')
+  async root () {
+    const docs = await this.userService.findAll({});
+    return {
+      data: docs
+    }
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from './config/config.module';
+import { Configuration } from './config';
+import { UserModule } from './shared/mongodb/user/user.module';
 
 @Module({
-  imports: [ConfigModule],
+  imports: [
+    MongooseModule.forRoot(Configuration.get('mongodb_url'), { useNewUrlParser: true }),
+    ConfigModule,
+    UserModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
